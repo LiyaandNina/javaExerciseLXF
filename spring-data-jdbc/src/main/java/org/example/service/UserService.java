8140473421ad4b97a340d2e5be64f4cd2029b21a
Gray <liyaandnina@gmail.com>
2023-11-28 10:11:01 +0800

    2023 11 27

---
+++ spring-data-jdbc/src/main/java/org/example/service/UserService.java
@@ -0,0 +1,103 @@
+package org.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+
+@Component
+public class UserService {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public User getUserById(long id) {
+        return jdbcTemplate.execute((Connection conn) -> {
+            try (PreparedStatement ps = conn.prepareStatement("SELECT * FROM users WHERE id = ?")) {
+                ps.setObject(1, id);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return new User(
+                                rs.getLong("id"),
+                                rs.getString("email"),
+                                rs.getString("password"),
+                                rs.getString("name")
+                        );
+                    }
+                    throw new RuntimeException("user not found by id");
+                }
+            }
+        });
+    }
+
+    public User getUserByName(String name) {
+        return jdbcTemplate.execute("SELECT * FROM users WHERE name = ?", (PreparedStatement ps) -> {
+            ps.setObject(1, name);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new User(
+                            rs.getLong("id"),
+                            rs.getString("email"),
+                            rs.getString("password"),
+                            rs.getString("name")
+                    );
+                }
+                throw new RuntimeException("user not found by name");
+            }
+        });
+    }
+
+    public User getUserByEmail(String email) {
+        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email = ?", (ResultSet rs, int RowNum) -> {
+            return new User(
+                    rs.getLong("id"),
+                    rs.getString("email"),
+                    rs.getString("password"),
+                    rs.getString("name")
+            );
+        }, email);
+    }
+
+    public long getUsers() {
+        return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users", (ResultSet rs, int RowNum) -> {
+            return rs.getLong(1);
+        });
+    }
+
+    public List<User> getUsers(int pageIndex) {
+        int limit = 100;
+        int offset = limit * (pageIndex - 1);
+        return jdbcTemplate.query("SELECT * FROM users LIMIT ? OFFSET ?",
+                new BeanPropertyRowMapper<>(User.class),
+                new Object[]{limit, offset});
+    }
+
+    public void updateUser(User user) {
+        if (1 != jdbcTemplate.update("UPDATE user SET name = ? WHERE id = ?", user.getName(), user.getId())){
+            throw new RuntimeException("user not found by id.");
+        }
+    }
+
+    public User register(String email, String password, String name) {
+        KeyHolder holder = new GeneratedKeyHolder();
+        if (1 != jdbcTemplate.update((conn) -> {
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO users(email, password, name) VALUES (?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            ps.setObject(1, email);
+            ps.setObject(2, password);
+            ps.setObject(3, name);
+            return ps;
+        }, holder)) {
+            throw new RuntimeException("user register failed.");
+        }
+        return new User(holder.getKey().longValue(), email, password, name);
+    }
+}
